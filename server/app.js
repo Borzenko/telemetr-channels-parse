a303f41beba248ccb277ccb4a280eaa582111221
Borzenko Mihail <borzenkomihail@yahoo.com>
2020-04-18 13:48:48 +0300

    Updated channels api

--- server/app.js
+++ server/app.js
@@ -27,4 +27,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.listen(3000, async () => {
+  console.log('Server is working in 3000')
+})
+
 module.exports = app;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const db = require('../db');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+router.get('/channels', async (req, res, next) => {
+  const channelsCollection = await db.collection('channels')
+  const perPage = 50
+  const page = req.query.page ? req.query.page : 1
+  
+  const items = await channelsCollection.find()
+    .skip(page > 0 ? ((page - 1) * perPage) : 0 )
+    .limit(50)
+    .toArray()
+  const total = await channelsCollection.find().count()
+  res.json({
+    results: items,
+    total
+  })
 });
 
 module.exports = router;
