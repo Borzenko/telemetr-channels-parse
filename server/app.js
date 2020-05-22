776c6642099f3d21778a70a368ce5c4e93dd04f9
Anatolii <a.a.bobrovnik@gmail.com>
2020-05-22 17:08:39 +0300

    fix res.render

--- server/app.js
+++ server/app.js
@@ -28,7 +28,11 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  //res.render('error');
+  res.json({
+    message: err.message,
+    error: err
+  });
 });
 
 app.listen(3000, async () => {
